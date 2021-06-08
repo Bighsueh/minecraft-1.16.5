7170e25cf0ff1cc4fd323e29fe6a08dab9949556
Hsueh <40841121@gm.nfu.edu.tw>
2021-06-08 10:51:00 +0800

    RegularSave 2021/06/08 週二 10:50:59.64

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 
-java -Xms2048M -Xmx24576M -server -XX:ParallelGCThreads=8 -jar forge-1.16.5-36.1.0.jar nogui
+java -Xms2048M -Xmx24576M -server -XX:ParallelGCThreads=8 -jar forge.jar nogui
 pause
